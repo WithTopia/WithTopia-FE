e09a7ede1af72a0d0c0ba4f38c358f281b93cc8b
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-10 19:43:26 +0900

    Update 웹소켓 기본 기능 구현 완료

--- src/components/chatBox/ChatBox.jsx
+++ src/components/chatBox/ChatBox.jsx
@@ -1,68 +1,20 @@
 import React, { useEffect, useRef , useState } from "react";
 import "../chat/Chat.scss"
-import axios from "axios";
-// import "../chatInputBox/ChatInputBox.scss"
 
-const url = process.env.REACT_APP_SERVER_URL2
-
-const ChatBox = ({ userData, except }) => {
-  const [data , setData] = useState([]) // 내가 친 채팅 및 유저관리
-  const [exceptData,setExceptData] = useState([]) // 남이 친 채팅
-  const [owner, setOwner] = useState(false)
-
+const ChatBox = ({ data ,userData }) => {
   const ref = useRef();
-
-  const update = {
-    user:except.sender,
-    message:except.message
-  }
-  
-  const compare = () =>{
-    if(userData.username === except.sender){
-      setExceptData([...exceptData,update])
-      setOwner(true)
-    }else if(userData.message===""){
-      console.log("반가워요")
-    }else if(userData.username !== except.sender){
-      setData([...exceptData,update])
-      setOwner(false) // 여기까지함
-    }
-  }  
-
-  useEffect(()=>{
-    if(except === ""){
-      console.log("nothing")
-    }
-    compare()
-  },[except])
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [data]);
 
   return (
-    <div className="messages">
-      <div
-        ref={ref}
-        className={"message" + (owner ? " owner" : "")}>
-        <div className="messageInfo">
-        </div>
-        <div className="messageContent">
-          <div className="message-left">
-            {exceptData.map((datas,index)=>{
-            return(
-              <div key={index}>{datas.user}
-                <p>{datas.message}</p>
-              </div>
-            )
-            })}
-          </div>
-          <div className="message-right">
-            {exceptData.map((datas,index)=>{
-            return(
-              <div key={index}>{datas.user}
-                <p>{datas.message}</p>
-              </div>
-            )
-            })}
-          </div>
-        </div>
+    <div
+      ref={ref}
+      className={`message ${data.user === userData.username && "owner"}`}>
+      <div className="messageInfo">
+      </div>
+      <div className="messageContent">{data.user}
+        {data.message === "" ? null : <p>{data.message}</p>}
       </div>
     </div>
   );
--- src/components/chatInputBox/ChatInputBox.jsx
+++ src/components/chatInputBox/ChatInputBox.jsx
@@ -1,24 +1,32 @@
 import ChatBox from "../chatBox/ChatBox";
-import { useState } from "react";
+import { useState ,useEffect } from "react";
 import Attach from "../../assets/attach.png";
 import Cam from "../../assets/cam.png";
 import Add from "../../assets/add.png";
 import More from "../../assets/more.png";
 import naga from "../../assets/naga.png";
 import axios from "axios";
-import "./ChatInputBox.scss"
 
 const url = process.env.REACT_APP_SERVER_URL2
 
 const ChatInputBox = ({userData,setUserData,roomId,stompClient,except,getOut,setGetOut}) => { // 채팅 인풋 박스
   String(roomId)
   const [img, setImg] = useState(null);
+  const [data , setData] = useState([]) // 내가 친 채팅 및 유저관리
+
+  const update = {
+    user:except.sender,
+    message:except.message
+  }
 
   const handleOut = async () => { //나가기 , 나중에 delete 될 예정
     try{
-      const repo = await axios.get(url+`/chat/room/${roomId}/exit`)
+      const repo = await axios.put(url+`/chat/room/${roomId}/exit`)
       console.log(repo)
-      getOut.unsubscribe()
+      // getOut.unsubscribe()
+      stompClient.disconnect({},function(){
+        console.log('연결 해제.')
+      })
     }catch(error){
       console.log(error)
     }
@@ -43,6 +51,15 @@ const ChatInputBox = ({userData,setUserData,roomId,stompClient,except,getOut,set
       const {value}= e.target;
       setUserData({...userData,"message": value});
   }
+  useEffect(()=>{
+    if(except.message === ""){
+      console.log("nothing")
+    }else{
+      setData([...data,update]) 
+    }
+    
+  },[except])
+  console.log(except)
   return(
     <div className="chat">
       <div className="chatInfo">
@@ -51,30 +68,33 @@ const ChatInputBox = ({userData,setUserData,roomId,stompClient,except,getOut,set
             <img src={Cam} alt="" />
             <img src={Add} alt="" />
             <img src={More} alt="" />
-            <a href='/main' onClick={()=>handleOut} className="getout"><img className="naga" src={naga} alt=""/></a>
+            <a href="/chat" onClick={()=>handleOut} className="getout"><img className="naga" src={naga} alt=""/></a>
         </div>
       </div>
-      <ChatBox userData={userData} except={except}></ChatBox>
+      <div className="messages">
+        {data.map((data, index)=>(
+          <ChatBox data={data} userData={userData} key={index}></ChatBox>
+        ))}
+      </div>
       <div className="input">
-          <input
-              type="text"
-              placeholder="Type something..."
-              onChange={handleMessage}
-              value={userData.message}
-          />
+        <input
+            type="text"
+            placeholder="Type something..."
+            onChange={handleMessage}
+            value={userData.message}
+        />
           <div className="send">
-              {/* <img src={Attach} alt="" /> */}
-              <input
-              type="file"
-              style={{ display: "none" }}
-              id="file"
-              onChange={(e) => setImg(e.target.files[0])}
-              />
-              <label htmlFor="file">
-              
-              <img src={Attach} alt="" />
-              </label>
-              <button onClick={sendMessage}>Send</button>
+            <input
+            type="file"
+            style={{ display: "none" }}
+            id="file"
+            onChange={(e) => setImg(e.target.files[0])}
+            />
+            <label htmlFor="file">
+            
+            <img src={Attach} alt="" />
+            </label>
+            <button onClick={sendMessage}>Send</button>
           </div>
         </div>
       </div>

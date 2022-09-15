7f24d8853c2a7fdd4d984d0e2a340f7f59cd3a7f
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-16 00:41:03 +0900

    build 테스트15

--- src/components/mainBox/mainBoxBar/MainBar.jsx
+++ src/components/mainBox/mainBoxBar/MainBar.jsx
@@ -10,7 +10,7 @@ const Mainbar = ({ datas }) => {
   const navigate = useNavigate()
   const enterRoom = async () => {
     try{
-      const repo = await axios.get(url+`/room/${datas.sessionId}`,{},{headers:{"Authorization":token}})
+      const repo = await axios.get(url+`/room/${datas.sessionId}`,null,{headers:{"Authorization":token}})
       console.log(repo)
       // navigate(`/room/${repo.data.data.sessionId}`,{state:{token:repo.data.data.token,sessionId:repo.data.data.sessionId,roomTitle:sendData.roomTitle}})
     }

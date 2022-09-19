36539e94b34318f8b158d4d6a41e4c1b73f9b9c6
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-19 11:02:24 +0900

    Build test12

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -22,7 +22,7 @@ const Room = () => {
   const [destroyedStream,setDestroyedStream] = useState("")
   const [checkMyScreen,setCheckMyScreen] = useState("")
   const [isConnect,setIsConnect] = useState(false)
-  
+  const [example,setExample] = useState(undefined)
   const deleteSubscriber = (streamManager) => {
     const prevSubscribers = subscribers;
     let index = prevSubscribers.indexOf(streamManager, 0);
@@ -31,127 +31,123 @@ const Room = () => {
       setSubscribers([...prevSubscribers]);
     }
   };
-  
-  const joinSession = () => {
-    setToken(location.state.token)  
-    console.log("토큰",location.state.token)
-    console.log("아이디",location.state.sessionId)
-    setSessionId(location.state.sessionId)  
-    // 1. openvidu 객체 생성
-    const newOV = new OpenVidu();
-    // socket 통신 과정에서 많은 log를 남기게 되는데 필요하지 않은 log를 띄우지 않게 하는 모드
-    newOV.enableProdMode();
-    // 2. initSesison 생성
-    const newSession = newOV.initSession();
-    setSession(newSession)
-    // JSON.parse(JSON.stringify(newSession))
-    // 3. 미팅을 종료하거나 뒤로가기 등의 이벤트를 통해 세션을 disconnect 해주기 위해 state에 저장
-    setOV(newOV);
-    // 4. session에 connect하는 과정
-    newSession.on('streamCreated', (e) => {
-      const newSubscriber = newSession.subscribe(
-        e.stream,
-        undefined
-        // JSON.parse(e.stream.connection.data).clientData
-      );
-      
-      const newSubscribers = subscribers;
-      newSubscribers.push(newSubscriber);
-      
-      setSubscribers([...subscribers,newSubscribers]);
-      setIsConnect(true)
-    });
-    // 1-3 예외처리
-    newSession.on('exception', (exception) => {
-      console.warn(exception);
-    });
-    // 1-2 session에서 disconnect한 사용자 삭제
-    newSession.on('streamDestroyed', (e) => {
-      if (e.stream.typeOfVideo === 'CUSTOM') {
-        deleteSubscriber(e.stream.streamManager);
-      } else {
-        setDestroyedStream(e.stream.streamManager);
-        setCheckMyScreen(true);
-      }
-    });
-    // 커넥팅 // 닉네임 받기~
-    setUsername(localStorage.getItem("nickname"))
-    newSession.connect( tokenStuff, { clientData: username})
-      .then(async () => {
-        await newOV.getUserMedia({
-          audioSource: false,
-          videoSource: undefined,
-          resolution: '240x180',
-          frameRate: 10,
-        })
-      .then((mediaStream) => {
-        let videoTrack = mediaStream.getVideoTracks()[0];
-        let newPublisher = newOV.initPublisher(
-          undefined,
-          {
-            audioSource: undefined,  // The source of audio. If undefined default audio input
-            videoSource: videoTrack, // The source of video. If undefined default video input
-            publishAudio: true,  // Whether you want to start the publishing with audio unmuted or muted
-            publishVideo: true, // Whether you want to start the publishing with video enabled or disabled
-            // resolution: '1280x720',  // The resolution of your video
-            // frameRate: 10,   // The frame rate of your video
-            insertMode: 'APPEND',  // How the video will be inserted according to targetElement
-            mirror: true   // Whether to mirror your local video or not
-          })
-          // 4-b user media 객체 생성
-          newSession.publish(newPublisher)
-          setPublisher(newPublisher)
-        })
-      })
-      .catch((error) => {
-        console.warn(
-          'There was an error connecting to the session:',
-          error.code,
-          error.message
-        );
-      });
-  }
 
-  useEffect(()=>{
-    window.addEventListener("beforeunload", onbeforeunload);
-    setTimeout(joinSession,1000)
-    return () => {
-      leaveSession()
-      // window.removeEventListener("beforeunload", onbeforeunload);
-    };
-  },[])
-
-  const leaveSession = () => {
-    // const mySession = session
-    console.log(session)
-    if(session){
-      session.disconnect()
-      console.log("나가 ㅋㅋ")
-    }
-    // session.unsubscribe(subscribers)
-    setSession(undefined);
-    setSubscribers([])
-    setSessionId("")
-    setOV(undefined)
-    setPublisher(undefined)
-    
-  }
   // 브라우저 새로고침, 종료, 라우트 변경
   const onbeforeunload = (e) => {
     e.preventDefault();
     e.returnValue = "";
-    console.log(session)
+    console.log("1")
     leaveSession();
   };
-
-  useEffect(() => {
-    window.onpopstate = () => {
-      history.push("/main");
-    };
-  },[]);
+  
   useEffect(()=>{
+    // window.addEventListener("beforeunload", onbeforeunload);
+    const joinSession = () => {
+      setToken(location.state.token)  
+      console.log("토큰",location.state.token)
+      console.log("아이디",location.state.sessionId)
+      setSessionId(location.state.sessionId)  
+      // 1. openvidu 객체 생성
+      const newOV = new OpenVidu();
+      // socket 통신 과정에서 많은 log를 남기게 되는데 필요하지 않은 log를 띄우지 않게 하는 모드
+      newOV.enableProdMode();
+      // 2. initSesison 생성
+      var newsession = newOV.initSession();
+      setSession(newsession)
+      // JSON.parse(JSON.stringify(newSession))
+      // 3. 미팅을 종료하거나 뒤로가기 등의 이벤트를 통해 세션을 disconnect 해주기 위해 state에 저장
+      setOV(newOV);
+      // 4. session에 connect하는 과정
+      newsession.on('streamCreated', (e) => {
+        const newSubscriber = newsession.subscribe(
+          e.stream,
+          undefined
+          // JSON.parse(e.stream.connection.data).clientData
+        );
+        
+        const newSubscribers = subscribers;
+        newSubscribers.push(newSubscriber);
+        
+        setSubscribers([...subscribers,newSubscribers]);
+        setIsConnect(true)
+      });
+      // 1-3 예외처리
+      newsession.on('exception', (exception) => {
+        console.warn(exception);
+      });
+      // 1-2 session에서 disconnect한 사용자 삭제
+      newsession.on('streamDestroyed', (e) => {
+        if (e.stream.typeOfVideo === 'CUSTOM') {
+          deleteSubscriber(e.stream.streamManager);
+        } else {
+          setDestroyedStream(e.stream.streamManager);
+          setCheckMyScreen(true);
+        }
+      });
+      // 커넥팅 // 닉네임 받기~
+      setUsername(localStorage.getItem("nickname"))
+      newsession.connect( tokenStuff, { clientData: username})
+        .then(async () => {
+          await newOV.getUserMedia({
+            audioSource: false,
+            videoSource: undefined,
+            resolution: '240x180',
+            frameRate: 10,
+          })
+        .then((mediaStream) => {
+          let videoTrack = mediaStream.getVideoTracks()[0];
+          let newPublisher = newOV.initPublisher(
+            undefined,
+            {
+              audioSource: undefined,  // The source of audio. If undefined default audio input
+              videoSource: videoTrack, // The source of video. If undefined default video input
+              publishAudio: true,  // Whether you want to start the publishing with audio unmuted or muted
+              publishVideo: true, // Whether you want to start the publishing with video enabled or disabled
+              // resolution: '1280x720',  // The resolution of your video
+              // frameRate: 10,   // The frame rate of your video
+              insertMode: 'APPEND',  // How the video will be inserted according to targetElement
+              mirror: true   // Whether to mirror your local video or not
+            })
+            // 4-b user media 객체 생성
+            newsession.publish(newPublisher)
+            setPublisher(newPublisher)
+          })
+        })
+        .catch((error) => {
+          console.warn(
+            'There was an error connecting to the session:',
+            error.code,
+            error.message
+        );
+        });
+    }
+    setTimeout(joinSession,1000)
+    
+    return () => {
+      // onbeforeunload()
+      leaveSession()
+      // window.removeEventListener("beforeunload", onbeforeunload);
+    };
+  },[])
+  const leaveSession = () => {
+    console.log("2")
     console.log(session)
-  },[session])
+    session.disconnect()
+    console.log("나가 ㅋㅋ")
+    // session.unsubscribe(subscribers)
+    // setSession(undefined);
+    setSubscribers([])
+    setSessionId("")
+    setOV(undefined)
+    setPublisher(undefined)
+  }
+  
+
+  // useEffect(() => {
+  //   window.onpopstate = () => {
+  //     history.push("/main");
+  //   };
+  // },[]);
   
   return (
     <div className='room'>
@@ -178,7 +174,7 @@ const Room = () => {
             <Tempo></Tempo>
           </div>
         </div>
-        <Link to="/main"><button>Emergency</button></Link>
+        <a href="/main"><button>Emergency</button></a>
       </div>
       {/* <Chat></Chat> */}
     </div>

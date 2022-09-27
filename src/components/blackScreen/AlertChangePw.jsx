865e5029268c827ea1da91503570ec4ebd84e5dc
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-27 16:52:26 +0900

    Update 비밀번호 입력 변경

--- src/components/blackScreen/AlertChangePw.jsx
+++ src/components/blackScreen/AlertChangePw.jsx
@@ -1,26 +1,62 @@
-import React from 'react'
-import "./AlertCreateRoom.scss"
+import axios from 'axios'
+import React, { useState } from 'react'
+import "./AlertChangePw.scss"
+import {useNavigate} from "react-router-dom"
 
 const AlertChangePw = ({page,setPage}) => {
-    const handleScreen = () => {
-        setPage((prev)=>!prev)
+  const navigate = useNavigate()
+  let token = localStorage.getItem("accessToken")
+  let refreshtoken = localStorage.getItem("refreshtoken")
+  const [password,setPassword] = useState({
+    origin:"",
+    newPassword:"",
+    newPasswordConfirm:""
+  })
+  const handleScreen = () => {
+      setPage((prev)=>!prev)
+  }
+  const submitHandle = async (e) => {
+    e.preventDefault()
+    try{
+      console.log(password)
+      const repo = await axios.put("/member/mypage/changepw",{
+      origin:password.origin,
+      password:password.newPassword,
+      passwordConfirm:password.newPasswordConfirm
+    },{headers:{"authorization":token,"refreshtoken":refreshtoken}})
+    console.log(repo)
+    if(repo.data.statusMsg === "정상"){
+      alert("변경 되었습니다.")
+      navigate("/main")
+      return
     }
-    const submitHandle = () => {
-
+    }catch(error){
+      if(error.response.data.errormessage ==="패스워드가 일치하지않습니다."){
+        alert("패스워드가 일치하지 않습니다.")
+      }
     }
-    return(
+  }
+  const handlePassword = (e) => {
+    setPassword({...password,origin:e.target.value})
+  }
+  const handlePassword2 = (e) => {
+    setPassword({...password,newPassword:e.target.value})
+  }
+  const handlePassword3 = (e) => {
+    setPassword({...password,newPasswordConfirm:e.target.value})
+  }
+  return(
     <div className='alert-change-pw'>
-        <div className='create-container'>
-        <div className='create-container-title'>비밀번호 변경</div>
-        <form className='create-room-form' onSubmit={submitHandle}>
-          <input placeholder=' ' type="text" className='room-input'></input>
-          <input placeholder=' 방 제목' type="text" className='room-input'></input>
-          <input placeholder=' 방 제목' type="text" className='room-input'></input>
-          <input placeholder=' 방 제목' type="text" className='room-input'></input>
-          <button>변경</button>
+        <div className='change-pw'>
+        <div className='change-pw-title'>비밀번호 변경</div>
+        <form className='change-pw-form'>
+          <input placeholder=' 기존 비밀번호' type="password" className='change-password-input' value={password.origin} onChange={handlePassword}></input>
+          <input placeholder=' 변경할 비밀번호' type="password" className='change-password-input' value={password.newPassword} onChange={handlePassword2}></input>
+          <input placeholder=' 비밀번호 확인' type="password" className='change-password-input' value={password.newPasswordConfirm} onChange={handlePassword3}></input>
+          <button className='change-pw-btn' onClick={submitHandle}>변경</button>
         </form>
       </div>
-      <div className='black-out' onClick={handleScreen}></div>
+      <div className='modal-out' onClick={handleScreen}></div>
     </div>
   )
 }
